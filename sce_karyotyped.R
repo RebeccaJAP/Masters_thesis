185cef6af26b1bcef53c8200b1b771835c017b47
RebeccaJAP <151630834+RebeccaJAP@users.noreply.github.com>
2024-07-25 15:24:19 +0300

    Update sce_karyotyped.R

--- sce_karyotyped.R
+++ sce_karyotyped.R
@@ -1,11 +1,10 @@
-.libPaths(c("/projappl/project_2010414/project_rpackages_4.3.2", .libPaths()))
-libpath <- .libPaths()[1]
+.libPaths(c("/projappl/project_2010414/project_rpackages_4.4.0", .libPaths()))
 
 library(SCEVAN)
 library(Seurat)
 
 
-# SCEVAN paper warns the results might be very bad if the initial choice of normal cells is wrong. Thus, more than one seed should be tested.
+  # SCEVAN paper warns the results might be very bad if the initial choice of normal cells is wrong. Thus, more than one seed should be tested.
 
 #set.seed(34)
 set.seed(97)
@@ -14,27 +13,27 @@ set.seed(97)
 dataname <- commandArgs(trailingOnly = TRUE)
 
 
-# Load either clustered or original data subsetted by sample and cluster / cell type.
-# Both files contain a Seurat object called "donor_subset_sample".
-# The object has all of the sample's donors but the "status" column has value "observation" for the main donor and "reference" for the others.
+  # Load either clustered or original data subsetted by sample and cluster / cell type.
+  # Both files contain a Seurat object called "donor_subset_sample".
+  # The object has all of the sample's donors but the "status" column has value "observation" for the main donor and "reference" for the others.
 
 load(paste0("/scratch/project_2010414/ot/saved/Method_tests/Karyotyped_donors/ref_annotated_data_clust/",dataname))
 # load(paste0("/scratch/project_2010414/ot/saved/Method_tests/Karyotyped_donors/ref_annotated_data_d20/",dataname))
 
 
-# Separate other donors' cells from the observations
+  # Separate other donors' cells from the observations
 
 reference <- donor_subset_sample[,which(donor_subset_sample$status=="reference")]
 observation <- donor_subset_sample[,which(donor_subset_sample$status=="observation")]
 
 
-# Create a dummy cell to use as the reference
+  # Create a dummy cell to use as the reference
 
 dummy_cell <- as(rowMeans(reference@assays$RNA$counts), "sparseMatrix")
 dimnames(dummy_cell) <- list(rownames(donor_subset_sample@assays$RNA$counts), c("reference_cell"))
 
 
-# Combine the matrices of counts of the dummy cell and the observations.
+  # Combine the matrices of counts of the dummy cell and the observations.
 
 donor_subset_sample_mat <- cbind(observation@assays$RNA$counts, dummy_cell)
 
