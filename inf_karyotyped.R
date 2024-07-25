2bfb7e841bdec6d6ebcd75059d247ffe042d8f1a
RebeccaJAP <151630834+RebeccaJAP@users.noreply.github.com>
2024-07-25 15:37:23 +0300

    Update inf_karyotyped.R

--- inf_karyotyped.R
+++ inf_karyotyped.R
@@ -6,27 +6,27 @@ library(Seurat)
 dataname <- commandArgs(trailingOnly = TRUE)
 
 
-  # Load either the original or the newly clustered data subsetted by sample and cell type/cluster.
+  # Load either the original or the newly clustered data subsetted by sample and cluster / cell type.
   # Both files contain a Seurat object called "donor_subset_sample".
   # The object has all of the sample's donors but the "status" column has value "observation" for the main donor and "reference" for the others.
 
-load(paste0("/scratch/project_2010414/ot/saved/Method_tests/Karyotyped_donors/ref_annotated_data/",dataname))
-# load(paste0("/scratch/project_2010414/ot/saved/Method_tests/Karyotyped_donors/ref_annotated_data_clust/",dataname))
+load(paste0("/scratch/project_2010414/ot/saved/Method_tests/Karyotyped_donors/ref_annotated_data_clust/",dataname))
+# load(paste0("/scratch/project_2010414/ot/saved/Method_tests/Karyotyped_donors/ref_annotated_data_d20/",dataname))
 
 
   # Create the annotations file for inferCNV:
 
 annotations <- subset(donor_subset_sample@meta.data, select=c(status))
 
-  # Add either the cell type name or cluster number to the annotation to make reading the results easier
-
-    # Cell type:
-cell_type <- donor_subset_sample$twoPassAnnotation_clean[1]
-annotations$status <- paste(annotations$status, cell_type, sep="_")
+  # Add either the cluster number or the cell type name to the annotation to make reading the results easier
 
     # Cluster:
-# cluster <- donor_subset_sample$seurat_clusters
-# annotations$status <- paste(annotations$status, cluster, sep="_")
+cluster <- donor_subset_sample$seurat_clusters
+annotations$status <- paste(annotations$status, cluster, sep="_")
+
+    # Cell type:
+# cell_type <- donor_subset_sample$twoPassAnnotation_clean[1]
+# annotations$status <- paste(annotations$status, cell_type, sep="_")
 
 
   # Create the inferCNV object
@@ -35,8 +35,8 @@ inf_donor_subset_sample = CreateInfercnvObject(raw_counts_matrix = donor_subset_
                                                 annotations_file = annotations
                                                 delim="\t",
                                                 gene_order_file = "/scratch/project_2010414/ot/saved/Method_tests/Sample OT_NI23-4_POOL3/hg38_gencode_v27.txt",
-                                                ref_group_names = c(paste0("reference_", cell_type)))
-                                              # ref_group_names = c(paste0("reference_", cluster)))      # Choose either this row or the one below according to the data used.
+                                                ref_group_names = c(paste0("reference_", cluster)))
+                                              # ref_group_names = c(paste0("reference_", cell_type)))      # Choose either this row or the one below according to the data used.
 
                                               
   # Run inferCNV
