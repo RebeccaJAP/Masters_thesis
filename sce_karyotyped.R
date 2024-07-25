661f42353c96f4af00555e674e1c374a50971d2b
RebeccaJAP <151630834+RebeccaJAP@users.noreply.github.com>
2024-07-25 15:31:20 +0300

    Update sce_karyotyped.R

--- sce_karyotyped.R
+++ sce_karyotyped.R
@@ -4,9 +4,10 @@ library(SCEVAN)
 library(Seurat)
 
 
-  # SCEVAN paper warns the results might be very bad if the initial choice of normal cells is wrong. Thus, more than one seed should be tested.
+  # SCEVAN paper warns the results might be very bad if the initial choice of normal cells is wrong.
+  # Thus, more than one seed should be tested.
 
-#set.seed(34)
+# set.seed(34)
 set.seed(97)
 
 
@@ -39,15 +40,15 @@ donor_subset_sample_mat <- cbind(observation@assays$RNA$counts, dummy_cell)
 
 
 sce_donor_subset_sample <- SCEVAN::pipelineCNA(donor_subset_sample_mat,
-                                               ClonalCN = TRUE,
+                                               ClonalCN = T,
                                                par_cores = 6,
                                                norm_cell = "reference_cell",
-                                            #  SUBCLONES = T,               # This makes sure the CNVs are analysed in addition to separating normal and non-normal cells from each other.
+                                            #  SUBCLONES = T,               # Make sure the CNVs are analysed in addition to separating normal and non-normal cells from each other.
                                             #  beta_vega = 0.5,             # Higher value results in coarser segmentation.
                                             #  AdditionalGeneSets =         # This could be used to add signatures of normal cells but I have not used it or found an example of using it.
                                                organism = "human",
                                                sample = paste0("V4_", dataname),
-                                               SCEVANsignatures = FALSE)    # This stops SCEVAN from using references other than the dummy cell.
+                                               SCEVANsignatures = F)    # Stop SCEVAN from using references other than the dummy cell.
 
 save(sce_donor_subset_sample, file=paste0("sce_results/V4_", dataname))
 
