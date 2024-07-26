723755cd54c98f9847029d4772e3c9734e9a97e9
RebeccaJAP <151630834+RebeccaJAP@users.noreply.github.com>
2024-07-26 12:03:49 +0300

    Update new_cell_types.R

--- clust_and_cell_types/new_cell_types.R
+++ clust_and_cell_types/new_cell_types.R
@@ -3,9 +3,9 @@
 library(Seurat)
 
 
-	# Load the reclustered Seurat object
+	# Load the Seurat object with new clusters
 
-load("/scratch/project_2010414/ot/saved/Reclustering/V4_obj.RData")
+load("/scratch/project_2010414/ot/saved/Reclustering/reclustered_obj.RData")
 
 
 	# Read the new cell types
@@ -13,14 +13,14 @@ load("/scratch/project_2010414/ot/saved/Reclustering/V4_obj.RData")
 cell_types <- read.csv("/scratch/project_2010414/ot/saved/desired_columns.csv")
 
 
-	# Matching is not needed: the order of cell types is the same as the order of the object
+	# Matching is not necessary here: the order of cell types is the same as the order of the object
 
-# cell_types_matched <- cell_types[match(cell_types$robustID, new_obj_V4$robustID),]
+# cell_types_matched <- cell_types[match(cell_types$robustID, obj$robustID),]
 
 
 	# Add the cell type column to the Seurat object meta data
 
-new_obj_V4@meta.data <- merge(new_obj_V4@meta.data, cell_types)
+obj@meta.data <- merge(obj@meta.data, cell_types)
 
 
-save(new_obj_V4, file=paste0("/scratch/project_2010414/ot/saved/Reclustering/V4_obj_cell_types.RData"))
+save(obj, file=paste0("/scratch/project_2010414/ot/saved/Reclustering/reclustered_obj_cell_types.RData"))
