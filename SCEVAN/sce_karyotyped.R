c5b340548ef5d6141789a398af306ac1038458a9
RebeccaJAP <151630834+RebeccaJAP@users.noreply.github.com>
2025-12-15 16:33:08 +0200

    Update sce_karyotyped.R

--- SCEVAN/sce_karyotyped.R
+++ SCEVAN/sce_karyotyped.R
@@ -1,54 +1,57 @@
-.libPaths(c("/projappl/project_2010414/project_rpackages_4.4.0", .libPaths()))
-
+  # Load packages
 library(SCEVAN)
 library(Seurat)
 
-
-  # SCEVAN paper warns the results might be very bad if the initial choice of normal cells is wrong.
-  # Thus, more than one seed should be tested.
-
-# set.seed(34)
 set.seed(97)
+
+  # Set the annotation type used:
+# annot <- "twopass"
+annot <- "marker"
+
 
 
 dataname <- commandArgs(trailingOnly = TRUE)
 
 
-  # Load either clustered or original data subsetted by sample and cluster / cell type.
+  # Load data subsetted by SampleID and cell type
+
+if (annot == "twopass") {
+   load(paste0("Karyotyped_donors/ref_annotated_data_d20/",dataname))}
+else (annot == "marker") {
+  load(paste0("Karyotyped_donors/ref_annotated_data_new_cell_types/",dataname))
+  }
+
   # Both files contain a Seurat object called "donor_subset_sample".
   # The object has all of the sample's donors but the "status" column has value "observation" for the main donor and "reference" for the others.
 
-load(paste0("/scratch/project_2010414/ot/saved/Method_tests/Karyotyped_donors/ref_annotated_data_clust/",dataname))
-# load(paste0("/scratch/project_2010414/ot/saved/Method_tests/Karyotyped_donors/ref_annotated_data_d20/",dataname))
 
-
-  # Separate other donors' cells from the observations
+  # Separate other cell lines' cells from the observations
 
 reference <- donor_subset_sample[,which(donor_subset_sample$status=="reference")]
 observation <- donor_subset_sample[,which(donor_subset_sample$status=="observation")]
 
 
-  # Create a dummy cell to use as the reference
+  # Pseudobulk the reference cells
 
 dummy_cell <- as(rowMeans(reference@assays$RNA$counts), "sparseMatrix")
 dimnames(dummy_cell) <- list(rownames(donor_subset_sample@assays$RNA$counts), c("reference_cell"))
 
 
-  # Combine the matrices of counts of the dummy cell and the observations.
+  # Combine the matrices of counts of the pseudobulk reference and the observation cells.
 
 donor_subset_sample_mat <- cbind(observation@assays$RNA$counts, dummy_cell)
 
+  # Run SCEVAN
 
 sce_donor_subset_sample <- SCEVAN::pipelineCNA(donor_subset_sample_mat,
                                                ClonalCN = T,
                                                par_cores = 6,
                                                norm_cell = "reference_cell",
-                                            #  SUBCLONES = T,               # Make sure the CNVs are analysed in addition to separating normal and non-normal cells from each other.
-                                            #  beta_vega = 0.5,             # Higher value results in coarser segmentation.
-                                            #  AdditionalGeneSets =         # This could be used to add signatures of normal cells but I have not used it or found an example of using it.
+                                               SUBCLONES = T,               # Make sure the CNVs are analysed in addition to separating normal and non-normal cells from each other.
+                                               beta_vega = 0.5,             # Higher value results in coarser segmentation.
                                                organism = "human",
-                                               sample = paste0("V4_", dataname),
-                                               SCEVANsignatures = F)    # Stop SCEVAN from using references other than the dummy cell.
+                                               sample = paste(dataname, annot, sep = "_"),
+                                               SCEVANsignatures = F)    # Stop SCEVAN from using references other than the pseudobulk reference input.
 
-save(sce_donor_subset_sample, file=paste0("sce_results/V4_", dataname))
-
+  # Save the results
+save(sce_donor_subset_sample, file=paste0("sce_results/", dataname, "_", annot))
