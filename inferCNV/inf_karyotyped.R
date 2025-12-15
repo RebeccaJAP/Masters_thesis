c2ce5f97783075cd4f5c4b2ccdd2cec857641dc5
RebeccaJAP <151630834+RebeccaJAP@users.noreply.github.com>
2025-12-15 16:58:21 +0200

    Update inf_karyotyped.R

--- inferCNV/inf_karyotyped.R
+++ inferCNV/inf_karyotyped.R
@@ -1,14 +1,30 @@
-.libPaths(c("/projappl/project_2010414/project_rpackages_4.4.0", .libPaths()))
-
+# Load packages
 library(infercnv)
 library(Seurat)
+
+set.seed(97)
+
+  # Set the annotation type used:
+# annot <- "twopass"
+annot <- "marker"
+
 
 dataname <- commandArgs(trailingOnly = TRUE)
 
 
-  # Load either the original or the newly clustered data subsetted by sample and cluster / cell type.
+  # Load data subsetted by SampleID and cell type
+
+if (annot == "twopass") {
+   load(paste0("Karyotyped_donors/ref_annotated_data_d20/",dataname))
+}
+else (annot == "marker") {
+  load(paste0("Karyotyped_donors/ref_annotated_data_new_cell_types/",dataname))
+}
+
+
   # Both files contain a Seurat object called "donor_subset_sample".
-  # The object has all of the sample's donors but the "status" column has value "observation" for the main donor and "reference" for the others.
+  # All cell lines of the pool and time point are included
+  # The cell line of interest has value "observation" in column "status", while the other cell lines' status is "reference".
 
 load(paste0("/scratch/project_2010414/ot/saved/Method_tests/Karyotyped_donors/ref_annotated_data_clust/",dataname))
 # load(paste0("/scratch/project_2010414/ot/saved/Method_tests/Karyotyped_donors/ref_annotated_data_d20/",dataname))
@@ -18,15 +34,16 @@ load(paste0("/scratch/project_2010414/ot/saved/Method_tests/Karyotyped_donors/re
 
 annotations <- subset(donor_subset_sample@meta.data, select=c(status))
 
-  # Add either the cluster number or the cell type name to the annotation to make reading the results easier
 
-    # Cluster:
-cluster <- donor_subset_sample$seurat_clusters
-annotations$status <- paste(annotations$status, cluster, sep="_")
+  # Add the cell type to the annotation to make reading the results easier
 
-    # Cell type:
-# cell_type <- donor_subset_sample$twoPassAnnotation_clean[1]
-# annotations$status <- paste(annotations$status, cell_type, sep="_")
+if (annot == "twopass") {
+   cell_type <- donor_subset_sample$twoPassAnnotation_clean[1]
+} else if (annot == "marker") {
+  cell_type <- donor_subset_sample$cell_type_pred_y[1]
+}
+
+annotations$status <- paste(annotations$status, cell_type, sep="_")
 
 
   # Create the inferCNV object
@@ -34,16 +51,15 @@ annotations$status <- paste(annotations$status, cluster, sep="_")
 inf_donor_subset_sample = CreateInfercnvObject(raw_counts_matrix = donor_subset_sample@assays$RNA$counts,
                                                 annotations_file = annotations,
                                                 delim="\t",
-                                                gene_order_file = "/scratch/project_2010414/ot/saved/Method_tests/Sample OT_NI23-4_POOL3/hg38_gencode_v27.txt",
-                                                ref_group_names = c(paste0("reference_", cluster)))
-                                              # ref_group_names = c(paste0("reference_", cell_type)))      # Choose either this row or the one below according to the data used.
+                                                gene_order_file = "hg38_gencode_v27.txt",
+                                                ref_group_names = c(paste0("reference_", cell_type))) 
 
                                               
   # Run inferCNV
 
 inf_donor_subset_sample = infercnv::run(inf_donor_subset_sample,
                                         cutoff=0.1,                          # Recommended value for 10x data
-                                        out_dir= paste0("/scratch/project_2010414/ot/saved/Method_tests/Karyotyped_donors/inf_results/V8_", dataname),
+                                        out_dir= paste0("Karyotyped_donors/inf_results/", dataname, "_", "annot"),
                                       # cluster_by_groups = F,               # Not relevant when only one observation group is run per time
                                         denoise=T,
                                       # BayesMaxPNormal = 0.5,               # Smaller value filters out more of the uncertain CNVs
